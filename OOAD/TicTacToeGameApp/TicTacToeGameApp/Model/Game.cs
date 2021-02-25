56aae221b4e281e151ecc6e3caa73b7c77b12941
skyras-infotech <skg1498@gmail.com>
2021-02-25 13:30:13 +0530

    Tic Tac Toe using OOPS

---
+++ OOAD/TicTacToeGameApp/TicTacToeGameApp/Model/Game.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGameApp.Model
+{
+    class Game
+    {
+        private static string[] _arr;
+        private int _flag;
+        private int _player;
+
+        public Game()
+        {
+            _arr = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+            _flag = 0;
+            _player = 1;
+        }
+
+        public string[] GetArray {
+            get { return _arr;  }
+        }
+
+
+        public int Player
+        {
+            get { return _player; }
+            set { _player = value; }
+        }
+
+        public int Flag
+        {
+            get { return _flag; }
+            set { _flag = value; }
+        }
+
+        private static bool CheckIfBoardIsFullOrNot()
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (_arr[i].Equals(i.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool CheckRows(string mark, int pos)
+        {
+            int start = 3 * (pos / 3);
+            for (int i = start; i < start + 3; i++)
+            {
+                if (_arr[i] != mark)
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool CheckColumn(string mark, int pos)
+        {
+            int start = pos % 3;
+            for (int i = start; i < 9; i += 3)
+            {
+                if (_arr[i] != mark)
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool CheckLeftToRightDiagonal(string mark, int pos)
+        {
+            int start = (pos % 3) - (pos / 3);
+            if (start != 0)
+            {
+                return false;
+            }
+            for (int i = start; i < 9; i += 4)
+            {
+                if (_arr[i] != mark)
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool CheckRightToLeftDiagonal(string mark, int pos)
+        {
+            int start = (pos % 3) + (pos / 3);
+            if (start != 2)
+            {
+                return false;
+            }
+            for (int i = start; i <= 6; i += 2)
+            {
+                if (_arr[i] != mark)
+                    return false;
+            }
+            return true;
+        }
+
+        public int CheckWin(string mark, int pos)
+        {
+            if (CheckRows(mark, pos))
+            {
+                return 1;
+            }
+            else if (CheckColumn(mark, pos))
+            {
+                return 1;
+            }
+            else if (CheckLeftToRightDiagonal(mark, pos))
+            {
+                return 1;
+            }
+            else if (CheckRightToLeftDiagonal(mark, pos))
+            {
+                return 1;
+            }
+            else if (CheckIfBoardIsFullOrNot())
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ OOAD/TicTacToeGameApp/TicTacToeGameApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeGameApp.Model;
+
+namespace TicTacToeGameApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            Console.WriteLine("========== Welcome to TicTacToe Game =========\n");
+            Console.Write("Enter 1st player name ==> ");
+            string player1 = Console.ReadLine();
+            Console.Write("Enter 2nd player name ==> ");
+            string player2 = Console.ReadLine();
+            Console.WriteLine("\n"+player1 + " your mark is X");
+            Console.WriteLine(player2 + " your mark is O"+"\n");
+            int pos;
+            Board(game.GetArray);
+            do
+            {
+                if (game.Player % 2 != 0)
+                {
+                    Console.Write(player1+", enter position you want to mark ==> ");
+                    pos = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.Write(player2 + ", enter position you want to mark ==> ");
+                    pos = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+
+                if (game.GetArray[pos] != "X" && game.GetArray[pos] != "O")
+                {
+                    if (game.Player % 2 == 0)
+                    { 
+                        game.GetArray[pos] = "O";
+                        game.Player++;
+                    }
+                    else
+                    {
+                        game.GetArray[pos] = "X";
+                        game.Player++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry the row {0} is already marked with {1}", pos, game.GetArray[pos]);
+                    Console.WriteLine("\n");
+                }
+                Board(game.GetArray);
+                game.Flag = game.CheckWin(game.GetArray[pos],pos);
+            } while (game.Flag != 1 && game.Flag != -1);
+
+            if (game.Flag == 1)
+            {
+                if (((game.Player % 2) + 1) % 2 != 0)
+                {
+                    Console.WriteLine(player1 + " has won");
+                }
+                else {
+                    Console.WriteLine(player2 + " has won");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("game is draw");
+            }
+        }
+        private static void Board(string[] arr)
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+            Console.WriteLine("     |     |      ");
+        }
+    }
+}
