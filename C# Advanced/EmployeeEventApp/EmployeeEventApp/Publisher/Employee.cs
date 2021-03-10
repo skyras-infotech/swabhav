213676c7d7953b51ca0f38d62aec1f7f02fe55a2
skyras-infotech <skg1498@gmail.com>
2021-03-10 09:38:06 +0530

    Fetching data from url with event

---
+++ C# Advanced/EmployeeEventApp/EmployeeEventApp/Publisher/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+
+namespace EmployeeEventApp.Publisher
+{
+    public delegate void ProcessingData();
+    class Employee
+    {
+        public event ProcessingData OnProcessingCompleted = null;
+
+        async public void ProcessData(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage responseMessage = await client.GetAsync(url))
+                {
+                    using (HttpContent content = responseMessage.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        Console.WriteLine("Data Loading ......\n"+data);
+                        OnProcessingCompleted?.Invoke();
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ C# Advanced/EmployeeEventApp/EmployeeEventApp/Subscriber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EmployeeEventApp.Publisher;
+
+namespace EmployeeEventApp.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.OnProcessingCompleted += DataLoaded;
+            employee.ProcessData("https://swabhav-tech.firebaseapp.com/emp.txt");
+            Console.ReadKey();
+        }
+
+        private static void DataLoaded() {
+            Console.WriteLine("\nData Processing is complete sucessfully...");
+        }
+       
+    }
+}
