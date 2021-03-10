3bf8b36c0db01c1499f77ecf67e1bb8d4757b535
skyras-infotech <skg1498@gmail.com>
2021-03-10 17:42:06 +0530

    Changed the folder structure

---
+++ Web/Javascript/type.js
@@ -0,0 +1,30 @@
+var foo = null;
+// console.log(foo);
+// document.writeln(foo);
+// alert(foo);
+
+// console.log(typeof(null));
+// console.log(typeof(undefined));
+
+// foo = 'abc'
+// console.log(foo);
+// document.writeln(foo);
+// console.log(typeof foo);
+// foo = 20;
+// console.log(typeof foo);
+// foo = true;
+// console.log(typeof foo);
+// foo = [];
+// console.log(typeof foo);
+// foo = {};
+// console.log(typeof foo);
+// foo = function () { };
+// console.log(typeof foo);
+
+
+console.log(document == window.document);
+console.log(location == window.location);
+console.log(console == window.console);   
+console.log(foo == window.foo);
+console.log(window);
+
