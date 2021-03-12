85a9d230fdeefa6046ea4dcc462795258b9409a1
skyras-infotech <skg1498@gmail.com>
2021-03-12 15:04:17 +0530

    Implement DOM manupulation

--- Web/Javascript/type.js
+++ Web/Javascript/type.js
@@ -1,25 +1,25 @@
 var foo = null;
-// console.log(foo);
-// document.writeln(foo);
-// alert(foo);
+console.log(foo);
+document.writeln(foo);
+alert(foo);
 
-// console.log(typeof(null));
-// console.log(typeof(undefined));
+console.log(typeof(null));
+console.log(typeof(undefined));
 
-// foo = 'abc'
-// console.log(foo);
-// document.writeln(foo);
-// console.log(typeof foo);
-// foo = 20;
-// console.log(typeof foo);
-// foo = true;
-// console.log(typeof foo);
-// foo = [];
-// console.log(typeof foo);
-// foo = {};
-// console.log(typeof foo);
-// foo = function () { };
-// console.log(typeof foo);
+foo = 'abc'
+console.log(foo);
+document.writeln(foo);
+console.log(typeof foo);
+foo = 20;
+console.log(typeof foo);
+foo = true;
+console.log(typeof foo);
+foo = [];
+console.log(typeof foo);
+foo = {};
+console.log(typeof foo);
+foo = function () { };
+console.log(typeof foo);
 
 
 console.log(document == window.document);
